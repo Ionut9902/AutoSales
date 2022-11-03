ff2da9dc4893ce91b949beb2293b3f17ace3f67f
Ionut <Ionut@DESKTOP-2PVK5AT>
2022-11-03 21:14:18 +0200

    Created Project, Updated databases, Scaffolded and created registration option

---
+++ AutoSales/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AutoSales.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
